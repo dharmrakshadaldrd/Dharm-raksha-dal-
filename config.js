3597ad11985412ce9b09619a7d31e5b408a27eb6
dharmrakshadaldrd <adityaraj72696@gmail.com>
2026-07-27 10:30:14 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  orgName: "धर्म रक्षा दल",
+  foundationName: "नमस्ते बिहार फाउंडेशन",
+  mobile: "+91 9304667439",
+  email: "your@email.com",
+  address: "समस्तीपुर, बिहार",
+  logo: "logo.png"
+};
